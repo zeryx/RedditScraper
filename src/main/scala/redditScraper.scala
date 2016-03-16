a7d73336d5a09b4b87e76d42a4b07556d9197692
zeryx <james.sutton1211@gmail.com>
2016-03-16 14:56:01 -0300

    made project compilable with cli args

--- src/main/scala/redditScraper.scala
+++ src/main/scala/redditScraper.scala
@@ -2,32 +2,30 @@
   * Created by james on 15/03/16.
   */
 
-import java.io.{InputStreamReader, BufferedReader, FileOutputStream, File}
-import java.net.{URLConnection, URL}
-import java.nio.channels.Channels
-import java.util.Scanner
+import java.io._
+import java.net.URL
 import javax.imageio.{IIOException, ImageIO}
 
 import org.apache.commons.io.{FilenameUtils, FileUtils}
 import play.api.libs.json._
-import scala.io.Source
 import scala.util.Try
 
 
-object redditScraper extends App{
+object redditScraper {
 
-  this.apply("Amateur", 1000)
+  def main(args: Array[String]): Unit = {
 
-
-  def apply(subreddit: String, totalNum: Int) = {
-
-    val dir = new File(s"/tmp/reddit//$subreddit/")
+    val dir = new File(s"/opt/reddit//${args(0)}/")
     dir.mkdirs()
 
+
+    //open a new csv file
+    val csvFile = new File(s"${dir.getAbsolutePath}/${args(0)}.csv")
     //makes a new request
     var last: String = ""
-    for (i <- 0 until totalNum by 20) {
-      val url = new URL(s"http://www.reddit.com/r/$subreddit/new.json?sort=new&after=$last")
+
+    for (i <- 0 until args(1).toInt by 20) {
+      val url = new URL(s"http://www.reddit.com/r/${args(0)}/new.json?sort=new&after=$last")
       val conn = url.openConnection()
       conn.setRequestProperty("User-Agent", "scala:com.algorithmia.redditImageScraper:v1.0.0 (by /u/zeryx1211)")
       conn.connect()
@@ -38,22 +36,34 @@ object redditScraper extends App{
 
       response.close()
 
+      val bw = new BufferedWriter(new FileWriter(csvFile, true))
+
       val items = (input \ "data" \ "children" \\ "data").toList
 
-      if(Try(last = (Json.parse(items.last.toString()) \ "name").get.toString.replaceAll("^\"|\"$", "")).isSuccess) {
+      if (Try(last = (Json.parse(items.last.toString()) \ "name").get.toString.replaceAll("^\"|\"$", "")).isSuccess) {
 
         val images = items.map((item: JsValue) => (Json.parse(item.toString()) \ "url").get.toString().replaceAll("^\"|\"$", ""))
+
 
         images.foreach((link: String) => {
           if (FilenameUtils.getExtension(link) == "jpg"
             || FilenameUtils.getExtension(link) == "png") {
             val file = new File(s"${dir.getAbsolutePath}/${FilenameUtils.getName(link)}")
             Try(FileUtils.copyURLToFile(new URL(link), file, 3000, 3000))
+            //write image url to csv file.
 
-            try{ImageIO.read(file)} catch{case e: IIOException =>  FileUtils.forceDelete(file)}
+            try {
+              ImageIO.read(file)
+              bw.write(s"$link,")
+              bw.newLine()
+            } catch {
+              case e: IIOException => FileUtils.forceDelete(file)
+            }
           }
         })
+        bw.close()
       }
+      Thread.sleep(500)
     }
   }
 }
