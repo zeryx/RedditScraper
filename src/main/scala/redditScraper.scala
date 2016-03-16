280e0865e8bb042b52a5b2469204a0d90aced5a2
zeryx <james.sutton1211@gmail.com>
2016-03-16 16:32:17 -0300

    added a try check to keep going if you recieve a 503 error.

--- src/main/scala/redditScraper.scala
+++ src/main/scala/redditScraper.scala
@@ -15,7 +15,7 @@ object redditScraper {
 
   def main(args: Array[String]): Unit = {
 
-    val dir = new File(s"/opt/reddit//${args(0)}/")
+    val dir = new File(s"${args(3)}/reddit/${args(0)}/")
     dir.mkdirs()
 
 
@@ -25,45 +25,48 @@ object redditScraper {
     var last: String = ""
 
     for (i <- 0 until args(1).toInt by 20) {
-      val url = new URL(s"http://www.reddit.com/r/${args(0)}/new.json?sort=new&after=$last")
-      val conn = url.openConnection()
-      conn.setRequestProperty("User-Agent", "scala:com.algorithmia.redditImageScraper:v1.0.0 (by /u/zeryx1211)")
-      conn.connect()
-      val response: BufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream))
+      try {
+        val url = new URL(s"http://www.reddit.com/r/${args(0)}/new.json?sort=new&after=$last")
+        val conn = url.openConnection()
+        conn.setRequestProperty("User-Agent", "scala:com.algorithmia.redditImageScraper:v1.0.0 (by /u/zeryx1211)")
+        conn.connect()
+        val response: BufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream))
 
 
-      val input = Json.parse(response.readLine())
+        val input = Json.parse(response.readLine())
 
-      response.close()
+        response.close()
 
-      val bw = new BufferedWriter(new FileWriter(csvFile, true))
+        val bw = new BufferedWriter(new FileWriter(csvFile, true))
 
-      val items = (input \ "data" \ "children" \\ "data").toList
+        val items = (input \ "data" \ "children" \\ "data").toList
 
-      if (Try(last = (Json.parse(items.last.toString()) \ "name").get.toString.replaceAll("^\"|\"$", "")).isSuccess) {
+        if (Try(last = (Json.parse(items.last.toString()) \ "name").get.toString.replaceAll("^\"|\"$", "")).isSuccess) {
 
-        val images = items.map((item: JsValue) => (Json.parse(item.toString()) \ "url").get.toString().replaceAll("^\"|\"$", ""))
+          val images = items.map((item: JsValue) => (Json.parse(item.toString()) \ "url").get.toString().replaceAll("^\"|\"$", ""))
 
 
-        images.foreach((link: String) => {
-          if (FilenameUtils.getExtension(link) == "jpg"
-            || FilenameUtils.getExtension(link) == "png") {
-            val file = new File(s"${dir.getAbsolutePath}/${FilenameUtils.getName(link)}")
-            Try(FileUtils.copyURLToFile(new URL(link), file, 3000, 3000))
-            //write image url to csv file.
+          images.foreach((link: String) => {
+            if (FilenameUtils.getExtension(link) == "jpg"
+              || FilenameUtils.getExtension(link) == "png") {
+              val file = new File(s"${dir.getAbsolutePath}/${FilenameUtils.getName(link)}")
+              Try(FileUtils.copyURLToFile(new URL(link), file, 3000, 3000))
+              //write image url to csv file.
 
-            try {
-              ImageIO.read(file)
-              bw.write(s"$link,")
-              bw.newLine()
-            } catch {
-              case e: IIOException => FileUtils.forceDelete(file)
+              try {
+                ImageIO.read(file)
+                bw.write(s"$link,")
+                bw.newLine()
+              } catch {
+                case e: IIOException => FileUtils.forceDelete(file)
+              }
             }
-          }
-        })
-        bw.close()
+          })
+          bw.close()
+        }
+      } catch {
+        case e: IOException => Thread.sleep(1000)
       }
-      Thread.sleep(500)
     }
   }
 }
